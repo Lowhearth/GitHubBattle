0f0441e0083a354169e2b6dabf3a62fda1c7d1d4
lowhearth <me@Mes-MacBook-Pro.local>
2018-07-15 03:45:53 +0200

    Added API Util and POPULAR grid display

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,5 @@
 var React = require ('react');
+var api = require ('../util/api');
 
 
 function SelectLanguage (props) {
@@ -22,6 +23,34 @@ function SelectLanguage (props) {
 
 }
 
+function RepoGrid (props){
+
+  return (
+    <ul className ="popular-list">
+      {props.repos.map((repo, index) => (
+        <li key = {index} className ="popular-item" >
+          <div className = 'popular-rank'># {index + 1}</div>
+          <ul>
+            <li>
+              <img  className='avatar' src ={repo.owner.avatar_url}/>
+            </li>
+            <li>
+              <a href={repo.html_url}>{repo.name}</a>
+            </li>
+            <li>
+              @{repo.owner.login}
+            </li>
+            <li>
+                {repo.stargazers_count} stars
+            </li>
+          </ul>
+        </li>
+      ))}
+    </ul>
+
+  )
+}
+
 
 
 class Popular extends React.Component {
@@ -29,25 +58,43 @@ class Popular extends React.Component {
   constructor(){
     super();
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos : null
     }
      this.updateLanguage = this.updateLanguage.bind(this);
   }
 
-  updateLanguage (lang){
-    this.setState(
-      {
-        selectedLanguage: lang
-      }
-    )
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
   }
+
+  updateLanguage(lang) {
+  this.setState(function () {
+    return {
+      selectedLanguage: lang,
+      repos: null
+    }
+  });
+
+  api.fetchPopularRepos(lang)
+    .then(function (repos) {
+      this.setState(function () {
+        return {
+          repos: repos
+        }
+      });
+    }.bind(this));
+}
 
   render(){
 
 
     return(
       <div>
+      <div>
         <SelectLanguage selectedLanguage = {this.state.selectedLanguage} onSelect = {this.updateLanguage}/>
+      </div>
+      { !this.state.repos ?  <p>loading</p> : <RepoGrid repos= {this.state.repos}/> }
       </div>
 
     )
